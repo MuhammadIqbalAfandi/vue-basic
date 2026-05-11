0a816765ce97f3d02ce65e0ce4bce83445442260
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2026-05-11 23:32:52 +0700

    computed properties

--- src/components/SayForm.vue
+++ src/components/SayForm.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import { reactive, type Reactive } from "vue";
+import {
+  computed,
+  reactive,
+  ref,
+  type ComputedRef,
+  type Reactive,
+  type Ref,
+} from "vue";
+
+const counter: Ref<number> = ref(0);
 
 const person: Reactive<{ firstName: string; lastName: string }> = reactive({
   firstName: "",
   lastName: "",
+});
+
+function increment(): void {
+  console.log("Counter before increment:", counter.value);
+  counter.value++;
+}
+
+const fullName: ComputedRef<string> = computed((prev) => {
+  console.log("Computing full name with previous value:", prev);
+  console.log(`${person.firstName} ${person.lastName}`);
+  return `${person.firstName} ${person.lastName}`;
 });
 
 function sayHello(): void {
@@ -17,6 +37,9 @@ function sayHello(): void {
 </script>
 
 <template>
+  <h1>Counter: {{ counter }}</h1>
+  <button @click="increment">Increment</button>
+
   <form @submit.prevent="sayHello">
     <input type="text" placeholder="Enter your name" id="firstName" />
     <br />
@@ -25,5 +48,5 @@ function sayHello(): void {
     <button type="submit">Say Hello</button>
   </form>
 
-  <h1>Hello, {{ person.firstName }} {{ person.lastName }}</h1>
+  <h1>Hello, {{ fullName }}</h1>
 </template>
