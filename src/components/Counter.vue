bb8be6bc941d9b51ce56e8d1537904bf89336d06
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2026-05-11 22:40:06 +0700

    state ref

---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+
+const count: Ref<number> = ref(0);
+
+function increment(): void {
+  count.value++;
+}
+</script>
+
+<template>
+  <h1>Counter: {{ count }}</h1>
+  <button @click="increment">Increment</button>
+</template>
