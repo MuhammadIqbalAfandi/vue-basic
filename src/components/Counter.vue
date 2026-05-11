c668d45c62ccb3fa30c9ba2ffa44c9f541438bec
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2026-05-11 22:47:44 +0700

    state object

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import { ref, type Ref } from "vue";
 
-const count: Ref<number> = ref(0);
+const count: Ref<{ id: number; name: string[] }> = ref({
+  id: 0,
+  name: [],
+});
 
 function increment(): void {
-  count.value++;
+  count.value.id++;
+  count.value.name.push(`Name ${count.value.id}`);
 }
 </script>
 
 <template>
-  <h1>Counter: {{ count }}</h1>
+  <h1>Counter: {{ count.id }} {{ count.name.join(", ") }}</h1>
   <button @click="increment">Increment</button>
 </template>
