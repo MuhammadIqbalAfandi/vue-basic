466c83ebee5fc65700bb075c712a9f88f231a15d
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2026-05-11 22:00:05 +0700

    template attribute

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 const hello: string = "<h1>Hello, World!</h1>";
+const classHello: string = "hello";
+const buttonDisabled: boolean = true;
+const styleHello: object = {
+  class: "hello",
+  id: "hello",
+};
 </script>
 
 <template>
-  <div class="hello">{{ hello }}</div>
-  <div v-html="hello"></div>
+  <div v-bind:class="classHello">{{ hello }}</div>
+  <div :class="classHello" v-html="hello"></div>
+  <div v-bind="styleHello">{{ hello }}</div>
+  <br />
+  <button :disabled="buttonDisabled">Click me</button>
 </template>
 
 <style scoped>
+#hello {
+  font-style: italic;
+}
 .hello {
   color: green;
 }
