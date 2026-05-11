8cb40f29e13d59ed68b4406c39947fc2ad8f74b1
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2026-05-11 22:06:17 +0700

    js expression

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,9 +6,16 @@ const styleHello: object = {
   class: "hello",
   id: "hello",
 };
+function helloWorld(): string {
+  return "Hello, World!";
+}
 </script>
 
 <template>
+  <h1>{{ 1997 + 1 }}</h1>
+  <h1>{{ helloWorld() }}</h1>
+  <h1>{{ hello.toUpperCase() }}</h1>
+
   <div v-bind:class="classHello">{{ hello }}</div>
   <div :class="classHello" v-html="hello"></div>
   <div v-bind="styleHello">{{ hello }}</div>
