9d621bc7c19438aa60d5954a844bc6d8625195d4
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2026-05-14 23:48:46 +0700

    slot

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import Container from "./components/Container.vue";
 import MyInputApp from "./components/MyInputApp.vue";
 </script>
 
 <template>
-  <MyInputApp />
+  <Container title="My Website">
+    <MyInputApp />
+  </Container>
 </template>
 
 <style></style>
---
+++ src/components/Container.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <p>Copyright 2024</p>
+  </footer>
+</template>
