d05ff3757a2793e7a4f9a983debe56e53c0cd9b2
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2026-05-14 14:10:03 +0700

    eager watchers

--- src/components/Watchers.vue
+++ src/components/Watchers.vue
@@ -1,39 +1,43 @@
 <script setup lang="ts">
 import { ref, watch, type Ref } from "vue";
 
-const productId: Ref<string> = ref("");
+const productId: Ref<string> = ref("product1");
 const product: Ref<{
-  id: number;
+  id: string;
   name: string;
   description: string;
   price: number;
 } | null> = ref(null);
 
-watch(productId, async (newVal, _) => {
-  if (newVal) {
-    try {
-      const response = await fetch(`/public/` + `product${newVal}.json`);
-      if (response.ok) {
-        product.value = await response.json();
-      } else {
-        console.error("Failed to fetch product data:", response.statusText);
+watch(
+  productId,
+  async (newVal, _) => {
+    if (newVal) {
+      try {
+        const response = await fetch(`/public/` + `${newVal}.json`);
+        if (response.ok) {
+          product.value = await response.json();
+        } else {
+          console.error("Failed to fetch product data:", response.statusText);
+          product.value = null;
+        }
+      } catch (error) {
+        console.error("Error fetching product data:", error);
         product.value = null;
       }
-    } catch (error) {
-      console.error("Error fetching product data:", error);
-      product.value = null;
     }
-  }
-});
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
   <label for="product"
     >Product Id:
     <select id="product" v-model="productId">
-      <option value="1">Product 1</option>
-      <option value="2">Product 2</option>
-      <option value="3">Product 3</option>
+      <option value="product1">Product 1</option>
+      <option value="product2">Product 2</option>
+      <option value="product3">Product 3</option>
     </select>
   </label>
 
@@ -41,8 +45,5 @@ watch(productId, async (newVal, _) => {
     <h1>{{ product.name }}</h1>
     <p>{{ product.description }}</p>
     <p>Price: ${{ product.price }}</p>
-  </div>
-  <div v-else>
-    <p>Please select a product to see its details.</p>
   </div>
 </template>
